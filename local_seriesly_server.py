c378fd4180acfd372013193abcaa9548eb08fc3c
erebos42 <martinw@ichmann.de>
2013-04-27 10:41:29 +0200

    Some fixes for the server. Added Upstart script for Ubuntu.

--- local_seriesly_server.py
+++ local_seriesly_server.py
@@ -11,7 +11,9 @@ from local_seriesly import LocalSeriesly
 
 PORT = 8070
 IP = '0.0.0.0'
-PATH = '/show'
+URI_PATH = '/show'
+FETCH_PATH = '/fetch'
+LS_PATH = '/opt/local_seriesly'
 
 # connection to local seriesly to generate html pages
 ls = LocalSeriesly()
@@ -27,7 +29,7 @@ def show_data(environ, start_response):
 
     # build html file path
     # TODO: fix os dependent parts
-    htmlfilepath = "./data/" + str(profile) + ".html"
+    htmlfilepath = LS_PATH + "/data/" + str(profile) + ".html"
 
     # open html file and put lines to output
     fd = open(htmlfilepath, 'r')
@@ -41,8 +43,17 @@ def show_data(environ, start_response):
     start_response(status, response_headers)
     return [ret]
 
+def fetch_data(environ, start_response):
+    ls.fetch()
+
+    # build response
+    status = '200 OK'
+    response_headers = [('Content-type','text/html')]
+    start_response(status, response_headers)
+    return ['data fetched...']
+
 # start wsgi server
-d = wsgiserver.WSGIPathInfoDispatcher({PATH: show_data})
+d = wsgiserver.WSGIPathInfoDispatcher({URI_PATH: show_data, FETCH_PATH: fetch_data})
 server = wsgiserver.CherryPyWSGIServer((IP, PORT), d)
 
 # signal callback
